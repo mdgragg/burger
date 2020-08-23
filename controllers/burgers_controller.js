2fe30bb360875a8a9920405e80ac3fe6d791afa8
Michael Gragg <contact@michaeldgragg.com>
2020-08-23 16:53:14 -0400

    added controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,6 @@
-var express = require("express");
-
-var router = express.Router();
-
-var burger = require("../models/burger.js");
+let express = require("express");
+let router = express.Router();
+let burger = require("../models/burger.js");
 
 // Routes
 
